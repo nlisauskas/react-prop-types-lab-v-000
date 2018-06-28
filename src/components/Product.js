5d6cec9f2a48bd81b3dab77bc65c5ed08944e07f
Nick Lisauskas <nlisausk@alumni.nd.edu>
2018-06-28 02:36:57 +0000

    Done.

--- src/components/Product.js
+++ src/components/Product.js
@@ -3,7 +3,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-class Product extends React.component {
+export default class Product extends React.Component {
   render() {
     return (
       <div className="product">
@@ -11,6 +11,7 @@ class Product extends React.component {
         <li>{this.props.name}</li>
         <li>{this.props.producer}</li>
         <li>{this.props.hasWatermark ? 'Watermarked' : 'Not watermaked'}</li>
+        <li>{this.props.hasWatermark}</li>
         <li>{this.props.color}</li>
         <li>{this.props.weight}</li>
       </ul>
